28c4902ab6cf1b687657ca854e7287c6766825f1
Jerry Ren <jerryexplodes@gmail.com>
2020-03-20 14:22:11 -0400

    ReactLog

--- LogInReactiveX/ViewController.swift
+++ LogInReactiveX/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var enabledLabel: UILabel!
+    
+    @IBOutlet weak var searchBarLabel: UISearchBar!
+    
+    
+    var loginViewModel = LogInViewModel()
+    let myDisposeBag = DisposeBag()
+            
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        _ = passwordTextField.rx.text.map { $0 ?? ""}.bind(to: loginViewModel.passwordText)
+        _ = emailTextField.rx.text.map{$0 ?? ""}.bind(to: loginViewModel.emailText)
+        
+        // _ = loginViewModel.emailText.bind(to: emailTextField.rx.text)
+        // this line is a backwards binding, sorta
+        
+        _ = loginViewModel.isValid.bind(to: logInButton.rx.isEnabled)
+        
+        loginViewModel.isValid.subscribe(onNext: {
+            isValid in
+            self.enabledLabel.text = isValid ? "Pass" : "Keep typing"
+            self.enabledLabel.textColor = isValid ? .systemGreen : .systemRed
+            print("isValid = \(isValid)")
+        }).disposed(by: myDisposeBag)
+        
+        
+    
+        
     }
 
 
---
+++ Pods/Target Support Files/Pods-LogInReactiveX/Pods-LogInReactiveX-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LogInReactiveX : NSObject
+@end
+@implementation PodsDummy_Pods_LogInReactiveX
+@end
