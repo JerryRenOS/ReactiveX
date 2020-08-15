75b43f6bb6ad1c3799ca0d8db39baa6ec103133c
Jerry Ren <yren@gm.slc.edu>
2020-08-15 15:59:36 -0400

    RxAPIDealer undeclared

---
+++ RxSplash.tmp/RxSplash/RxAPIDealer.swift
@@ -0,0 +1,25 @@
+//
+//  RxAPIDealer.swift
+//  RxSplash
+//
+//  Created by Jerry Ren on 8/15/20.
+//  Copyright © 2020 Jerry Ren. All rights reserved.
+//
+
+import Foundation
+
+public func callingAPIFromRxAPIDealer(with urlString: String) -> Observable<Data?> {
+            
+        Observable<Data?>.create {  observer  in
+            URLSession.shared.dataTask(with: URL(string: urlString)!) { (data, response, erro) in
+                observer.onNext(data)
+                if erro != nil {
+                    observer.onError(erro!)
+                }
+                observer.onCompleted()
+            }.resume()
+            return Disposables.create()
+        }
+    }
+
+}
