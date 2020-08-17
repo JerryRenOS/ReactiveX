8498644ccf17f6eec6e91b03657ce02535ae9220
Jerry Ren <yren@gm.slc.edu>
2020-08-16 23:59:22 -0400

    im-lessness

--- RxGraphicsInterFi.tmp/RxGraphicsInterFi/GraphicsInterFiViewController.swift
+++ RxGraphicsInterFi.tmp/RxGraphicsInterFi/GraphicsInterFiViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import RxCocoa
 import RxSwift
 
+// Make 
+
 class GraphicsInterFiViewController: UIViewController {
     
     let disposeBaggy = DisposeBag()
--- RxGraphicsInterFi.tmp/RxGraphicsInterFi/StorageCloset.swift
+++ RxGraphicsInterFi.tmp/RxGraphicsInterFi/StorageCloset.swift
@@ -11,7 +11,12 @@ import Foundation
 struct StorageCloset {
     static let goofballdeveloperKey = "cqgYNwdFsLYkNQyBWXRNGPtKzWuR5NfG"
     
-    
     static let trumpURLForFun = "https://api.giphy.com/v1/gifs/search?api_key=cqgYNwdFsLYkNQyBWXRNGPtKzWuR5NfG&q=trump&limit=13&offset=0&rating=r&lang=en"
     
 }
+
+
+// kim jon en haha
+//https://media2.giphy.com/media/3jGewqNYjuPhm/giphy.gif?cid=34d60d39q4ljhjtfumrhp0llsrw9zxzzvrc0dvwiqlzlxcst&rid=giphy.gif
+
+// https://giphy.com/gifs/sbnation-2Fazg4XrLLmoGqwMg
